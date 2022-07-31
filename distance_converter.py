3027f20f2d2de9233093d915528133b020464fd3
Tabi Bean <89669679+TabiBean@users.noreply.github.com>
2022-07-31 12:18:37 -0500

    Created distance Converter
    Kilometers to Miles in python

---
+++ distance_converter.py
@@ -0,0 +1,8 @@
+km= float(input("How many kilometers did you decide to run today?\n"))
+
+print(f"Ok, you said {km} kilometers.\n")
+
+miles = km/1.60934
+miles = round(miles, 2)
+print(f"Your {km} kilometers run was {miles} miles.\n")
+
